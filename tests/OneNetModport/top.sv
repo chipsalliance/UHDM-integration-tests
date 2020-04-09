ef6fd60d9eadf53aec0f1d1da3824a64e8c17837
Rafal Kapuscik <rkapuscik@antmicro.com>
2020-04-09 16:25:53 +0200

    Update OneNetModport test
    Signed-off-by: Rafal Kapuscik <rkapuscik@antmicro.com>

--- tests/OneNetModport/top.sv
+++ tests/OneNetModport/top.sv
@@ -1,13 +1,13 @@
 // Source:
 // https://github.com/alainmarcel/Surelog/tree/master/tests/OneNetModPort
 
-program TESTBENCH  (ConnectTB.tb intf);
-  initial begin
-    $monitor("@%0dns observe = %0d",$time,intf.observe);
-    intf.drive = 0;
-    #100 intf.drive = 1;
-  end
-endprogram
+//program TESTBENCH  (ConnectTB.tb intf);
+//  initial begin
+////    $monitor("@%0dns observe = %0d",$time,intf.observe);
+//    intf.drive = 0;
+//    #100 intf.drive = 1;
+//  end
+//endprogram
 
 
 interface ConnectTB;
@@ -31,8 +31,10 @@ module SUB (input wire inp, output reg out);
   assign out = inp;
 endmodule
 
-module TOP();
+module top(input wire a, output reg b);
   ConnectTB conntb();
+  assign conntb.drive = a;
+  assign b = conntb.observe;
   DUT dut(conntb);
-  TESTBENCH tb(conntb);
+//  TESTBENCH tb(conntb);
 endmodule
