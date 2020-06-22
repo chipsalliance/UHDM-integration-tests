5a2f07a36d4f9cfee9ee354330998fe25e248587
Rafal Kapuscik <rkapuscik@antmicro.com>
2020-06-22 11:53:29 +0200

    Report node frequency in coverage report
    Signed-off-by: Rafal Kapuscik <rkapuscik@antmicro.com>

--- gen_coverage_report.py
+++ gen_coverage_report.py
@@ -10,6 +10,7 @@ args = parser.parse_args()
 
 present_in_uhdm = set()
 visited_by_frontend = set()
+unhandled_node_frequency = dict()
 
 with open(args.verilator_report, 'r') as f:
     target = present_in_uhdm
@@ -20,11 +21,17 @@ with open(args.verilator_report, 'r') as f:
             target = visited_by_frontend
         else:
             target.add(line)
+            # Update occurrences count for node type, remove endline from name
+            node_type = line.split(':')[-1][:-1]
+            unhandled_node_frequency[node_type] = unhandled_node_frequency.get(node_type, 0) + 1
 
 with open(args.output_file, 'w') as outfile:
     not_visited = present_in_uhdm - visited_by_frontend
     arranged = sorted(not_visited)
     outfile.write(''.join(arranged))
-    outfile.write("Present in UHDM: " + str(len(present_in_uhdm)))
-    outfile.write("Visited: " + str(len(visited_by_frontend)))
-    outfile.write("Overall coverage: " + str(len(visited_by_frontend)/len(present_in_uhdm)*100) + "%")
+    outfile.write("Present in UHDM: " + str(len(present_in_uhdm)) + '\n')
+    outfile.write("Visited: " + str(len(visited_by_frontend)) + '\n')
+    outfile.write("Overall coverage: " + str(len(visited_by_frontend)/len(present_in_uhdm)*100) + "%\n")
+    outfile.write("\nMissing node count per type:\n")
+    for node in sorted(unhandled_node_frequency, key=unhandled_node_frequency.get, reverse=True):
+        outfile.write("  Type: " + str(node) + "\tCount: " + str(unhandled_node_frequency[node]) + '\n')
