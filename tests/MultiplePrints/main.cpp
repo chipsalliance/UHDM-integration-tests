ed50eaff4d7bcbebf3c74243d77e7c652df9c3d9
Rafal Kapuscik <rkapuscik@antmicro.com>
2020-10-28 09:24:20 +0100

    Add MultiplePrints test
    Signed-off-by: Rafal Kapuscik <rkapuscik@antmicro.com>

---
+++ tests/MultiplePrints/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <verilated_vcd_c.h>
+
+#define VL_DEBUG
+#include "Vwork_top.h"
+#include "verilated.h"
+
+static vluint64_t main_time = 0;
+
+double
+sc_time_stamp()
+{
+  return main_time;
+}
+
+int main (int argc, char **argv) {
+  Verilated::commandArgs(argc, argv);
+  Vwork_top *top = new Vwork_top();
+
+  Verilated::traceEverOn(true);
+  VerilatedVcdC* tfp = new VerilatedVcdC;
+  top->trace(tfp, 99);
+  tfp->open("dump.vcd");
+
+  top->i = 0;
+  while (!Verilated::gotFinish() && (main_time < 100)) {
+    top->eval();
+    tfp->dump(main_time);
+
+    main_time += 1;
+
+    // We expect $displays from top.sv
+
+  }
+  top->final();
+  tfp->close();
+    delete top;
+
+  return 0;
+}
