83eafb296e976500d3c101444b0b1816391862c5
Rafal Kapuscik <rkapuscik@antmicro.com>
2020-11-05 15:02:21 +0100

    Add files for simulation
    Signed-off-by: Rafal Kapuscik <rkapuscik@antmicro.com>

---
+++ tests/EnumInPackage/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <verilated_vcd_c.h>
+
+#define VL_DEBUG
+#include "Vwork_dut.h"
+#include "verilated.h"
+
+static vluint64_t main_time = 0;
+
+double
+sc_time_stamp()
+{
+  return main_time;
+}
+
+int main (int argc, char **argv) {
+  Verilated::commandArgs(argc, argv);
+  Vwork_dut *top = new Vwork_dut();
+
+  Verilated::traceEverOn(true);
+  VerilatedVcdC* tfp = new VerilatedVcdC;
+  top->trace(tfp, 99);
+  tfp->open("dump.vcd");
+
+  while (!Verilated::gotFinish() && (main_time < 100)) {
+    top->eval();
+    tfp->dump(main_time);
+
+    main_time += 1;
+
+    std::cout << "time: " << main_time
+      //<< " a: " << (top->var1?1:0)
+      //<< " b: " << (top->var2?1:0)
+      << std::endl;
+  }
+  top->final();
+  tfp->close();
+    delete top;
+
+  return 0;
+}
