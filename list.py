694a7323310bb92b7420cefbbd15f4d9b88f392f
Kamil Rakoczy <krakoczy@antmicro.com>
2020-12-16 09:22:03 +0100

    Add generation of test matrix for ibex-modules
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- list.py
+++ list.py
@@ -4,7 +4,6 @@ import argparse
 import json
 import os
 import os.path
-import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--directory', '-d', type=str,
@@ -16,5 +15,8 @@ parser.add_argument('--skip-elements', '-s',
 
 args = parser.parse_args()
 
+if args.directory[-1] == "/":
+    args.directory = args.directory[:-1]
 # skip ibex test as it is handled separately
-print(json.dumps([f"tests/{node}" for node in os.listdir(args.directory) if node not in args.skip_elements]))
+print(json.dumps([f"{args.directory}/{node}" for node in os.listdir(args.directory)
+    if node not in args.skip_elements and os.path.isdir(args.directory + "/" + node)]))
