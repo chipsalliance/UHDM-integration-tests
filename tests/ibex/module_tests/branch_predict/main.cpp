01ca129cd49f62ad89efdd84316de8ceec787b9c
Rafal Kapuscik <rkapuscik@antmicro.com>
2021-01-07 14:45:33 +0100

    Add test for ibex_branch_predict
    Signed-off-by: Rafal Kapuscik <rkapuscik@antmicro.com>

---
+++ tests/ibex/module_tests/branch_predict/main.cpp
@@ -0,0 +1,46 @@
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include "Vibex_branch_predict.h"
+
+vluint64_t main_time = 0;
+
+int main(int argc, char* argv[])
+{
+  Verilated::traceEverOn(true);
+  VerilatedVcdC* tfp = new VerilatedVcdC;
+
+  Vibex_branch_predict* top = new Vibex_branch_predict;
+  top->trace(tfp, 99);
+  tfp->open("dump.vcd");
+
+  top->clk_i = 0;
+  top->rst_ni = 0;
+
+  while (!Verilated::gotFinish()) {
+    if (main_time >= 5000) break;
+
+    if (main_time && main_time % 5 == 0) top->clk_i = !top->clk_i;
+    switch (main_time) {
+    case 100: top->rst_ni = 1; break;
+
+    case 200:
+      top->fetch_rdata_i = 0x6f;
+      top->fetch_pc_i = 0x01234567;
+      break;
+    case 300:
+      top->fetch_valid_i = 1;
+      break;
+
+    default: break;
+    }
+
+    top->eval();
+    tfp->dump(main_time);
+    ++main_time;
+  }
+
+  top->final();
+  tfp->close();
+
+  return 0;
+}
