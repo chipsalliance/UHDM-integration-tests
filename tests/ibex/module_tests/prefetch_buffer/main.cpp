cd0265be8759007a58774df43de8e90d84026a03
Rafal Kapuscik <rkapuscik@antmicro.com>
2021-01-13 11:31:17 +0100

    Add test for ibex_prefetch_buffer
    Signed-off-by: Rafal Kapuscik <rkapuscik@antmicro.com>

---
+++ tests/ibex/module_tests/prefetch_buffer/main.cpp
@@ -0,0 +1,46 @@
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include "Vibex_prefetch_buffer.h"
+
+vluint64_t main_time = 0;
+
+int main(int argc, char* argv[])
+{
+  Verilated::traceEverOn(true);
+  VerilatedVcdC* tfp = new VerilatedVcdC;
+
+  Vibex_prefetch_buffer* top = new Vibex_prefetch_buffer;
+  top->trace(tfp, 99);
+  tfp->open("dump.vcd");
+
+  top->clk_i = 0;
+  top->rst_ni = 0;
+
+  while (!Verilated::gotFinish()) {
+    if (main_time >= 5000) break;
+
+    if (main_time && main_time % 5 == 0) top->clk_i = !top->clk_i;
+    switch (main_time) {
+    case 100: top->rst_ni = 1; break;
+
+    case 200:
+      top->addr_i = 0x12345678;
+      top->instr_rdata_i = 63;
+      top->instr_rvalid_i = 1;
+      top->ready_i = 1;
+      top->branch_i = 1;
+      break;
+
+    default: break;
+    }
+
+    top->eval();
+    tfp->dump(main_time);
+    ++main_time;
+  }
+
+  top->final();
+  tfp->close();
+
+  return 0;
+}
