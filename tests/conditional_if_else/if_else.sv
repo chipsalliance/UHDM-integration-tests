e0d9be175fcb0450fac62bba550218914b1fca4a
Paweł Czarnecki <pczarnecki@antmicro.com>
2021-03-25 14:26:15 +0100

    if-else: modify to return sane vcds
    Signed-off-by: Paweł Czarnecki <pczarnecki@antmicro.com>

--- tests/conditional_if_else/if_else.sv
+++ tests/conditional_if_else/if_else.sv
@@ -3,9 +3,9 @@
 :description: A module testing if-else statement
 :tags: 12.4
 */
-module if_tb ();
-	wire a = 0;
-	reg b = 0;
+module top (b);
+	wire a = 1;
+	output reg b = 0;
 	always @* begin
 		if(a) b = 1;
 		else b = 0;
