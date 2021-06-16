be506e4b78276c4e3078ef7e00c443151704e904
Krzysztof Bieganski <kbieganski@antmicro.com>
2021-06-16 14:36:10 +0200

    Fix TypedefAlias test until better support in Surelog

--- tests/TypedefAlias/dut.v
+++ tests/TypedefAlias/dut.v
@@ -11,13 +11,14 @@ package pkg;
      logic[7:0] x;
   } struct_t;
 
-  typedef struct_t second_alias_t;
-
   typedef logic [7:0] third_alias_t;
 endpackage;
 
 module dut (input pkg::enum_t a, output pkg::alias_t b);
-   pkg::second_alias_t c;
+   typedef pkg::struct_t second_alias_t; // Can be moved to pkg after
+                                         // https://github.com/chipsalliance/Surelog/issues/1388
+
+   second_alias_t c;
    pkg::third_alias_t d;
    assign c = a;
    assign d = c;
