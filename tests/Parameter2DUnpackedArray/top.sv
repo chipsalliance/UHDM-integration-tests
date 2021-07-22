ca6fb2f78c4bad2f014c788d4a419e3eb6174624
Ryszard Różak <rrozak@antmicro.com>
2021-07-22 11:13:52 +0200

    Assign different value to each element
    Signed-off-by: Ryszard Różak <rrozak@antmicro.com>

--- tests/Parameter2DUnpackedArray/top.sv
+++ tests/Parameter2DUnpackedArray/top.sv
@@ -1,5 +1,5 @@
-module top(output logic o);
-   parameter logic P [1:0][2:0] = '{'{1, 1, 1},
-                                    '{1, 1, 1}};
+module top(output int o);
+   parameter int P [1:0][2:0] = '{'{0, 1, 2},
+                                  '{10, 11, 12}};
    assign o = P[1][1];
 endmodule
