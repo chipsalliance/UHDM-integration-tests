9cc48a2575c561edddd9cd09963c28aeffb08a1f
Ryszard Różak <rrozak@antmicro.com>
2021-07-23 10:20:31 +0200

    Add ParameterUnpackedLogicArray test
    Signed-off-by: Ryszard Różak <rrozak@antmicro.com>

---
+++ tests/ParameterUnpackedLogicArray/top.sv
@@ -0,0 +1,15 @@
+module top(output logic a, b, c, d);
+   parameter logic P [1:0][2:0][3:0] = '{
+				       '{'{0, 1, 0, 1},
+					 '{0, 1, 1, 1},
+					 '{0, 1, 1, 1}},
+				       '{'{0, 1, 1, 1},
+					 '{0, 1, 1, 1},
+					 '{0, 1, 1, 1}}
+				       };
+   
+   assign a = P[0][0][2];
+   assign b = P[0][1][3];
+   assign c = P[1][2][1];
+   assign d = P[1][0][3];   
+endmodule
