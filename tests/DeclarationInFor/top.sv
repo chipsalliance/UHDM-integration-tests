017c9685a4d6e3156d0968ecb8230c0b40f5122f
Ryszard Różak <rrozak@antmicro.com>
2021-07-26 16:24:22 +0200

    Add DeclarationInFor test
    Signed-off-by: Ryszard Różak <rrozak@antmicro.com>

---
+++ tests/DeclarationInFor/top.sv
@@ -0,0 +1,10 @@
+module top(output logic o);
+   function automatic logic theta();
+      for (int x = 0 ; x < 5 ; x++) begin
+         int a;
+      end
+      return 1'b1;
+   endfunction : theta
+
+   assign o = theta();
+endmodule : top
