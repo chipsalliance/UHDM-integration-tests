c196235286100e4aae4fad83c7f3646faafad93f
Ryszard Różak <rrozak@antmicro.com>
2021-07-28 09:53:45 +0200

    Fix indentation
    Signed-off-by: Ryszard Różak <rrozak@antmicro.com>

--- tests/DeclarationInFor/top.sv
+++ tests/DeclarationInFor/top.sv
@@ -2,8 +2,8 @@ module top(output logic o);
    function automatic logic theta();
       for (int x = 0 ; x < 5 ; x++) begin
          logic a;
-	 a = 1'b1;
-	 return a;
+         a = 1'b1;
+         return a;
       end
       return 1'b0;
    endfunction : theta
