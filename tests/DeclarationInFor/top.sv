e88852a9a590226bed68fe161aacb640af21f506
Ryszard Różak <rrozak@antmicro.com>
2021-07-28 09:49:19 +0200

    Add using 'a' variable
    Signed-off-by: Ryszard Różak <rrozak@antmicro.com>

--- tests/DeclarationInFor/top.sv
+++ tests/DeclarationInFor/top.sv
@@ -1,9 +1,11 @@
 module top(output logic o);
    function automatic logic theta();
       for (int x = 0 ; x < 5 ; x++) begin
-         int a;
+         logic a;
+	 a = 1'b1;
+	 return a;
       end
-      return 1'b1;
+      return 1'b0;
    endfunction : theta
 
    assign o = theta();
