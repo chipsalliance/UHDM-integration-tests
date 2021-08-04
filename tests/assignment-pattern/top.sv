a58f0d362ad57c050ab8816e91a023ab545d01a0
Ryszard Różak <rrozak@antmicro.com>
2021-08-04 14:47:11 +0200

    Fix assignment-pattern test
    Signed-off-by: Ryszard Różak <rrozak@antmicro.com>

--- tests/assignment-pattern/top.sv
+++ tests/assignment-pattern/top.sv
@@ -1,33 +1,38 @@
 package p;
-	typedef struct packed {
-		integer r;
-		integer th;
-	} C;
+   typedef struct packed {
+      integer 	  r;
+      integer 	  th;
+   } C;
 
-	typedef struct packed {
-		logic [63:0] [3:0] a;
-	} S;
+   typedef struct packed {
+      logic [2:0][63:0] a;
+   } S;
 endpackage
 
-module top(input [3:0] x, output reg [3:0] y, z);
-	integer k = '{0:1, default: 0};
-	integer i = '{31:1, 23:1, 15:1, 8:1, default: 0};
-	integer j = '{default: 1, 31:0};
+module top(output integer i, j, k, l_r, l_th,
+	   output logic [63:0] m1, m2, m3,
+	   output logic [2:0]  n);
 
-	p::C l = '{th:170, r:1};
+   assign i = '{31:1, 23:1, 15:1, 8:1, default: 0};
+   assign j = '{default: 1, 31:0};
+   assign k = '{0:1, default: 0};
 
-	logic [96:0] A = '{1, 2, 3};
+   p::C l = '{th:170, r:1};
+   assign {l_r, l_th} = l;
 
-	p::S m = '{64'h9, 64'h12, 64'h21};
+   p::S m = '{'{64'h9, 64'h12, 64'h21}};
+   assign {m1, m2, m3} = m;
 
-	always_comb begin
-		assert(32'b00000000000000000000000000000001 == k);
-		assert(32'b10000000100000001000000100000000 == i);
-		assert(32'b01111111111111111111111111111111 == j);
+   assign n = '{1, 1, 0};
 
-		assert(l == 64'b0000000000000000000000000000000100000000000000000000000010101010);
-		assert(m == 192'b000000000000000000000000000000000000000000000000000000000000100100000000000000000000000000000000000000000000000000000000000100100000000000000000000000000000000000000000000000000000000000100001);
+   always_comb begin
+      assert(i == 32'b10000000100000001000000100000000);
+      assert(j == 32'b01111111111111111111111111111111);
+      assert(k == 32'b00000000000000000000000000000001);
 
-		assert(A == 96'b000000000000000000000000000000010000000000000000000000000000001000000000000000000000000000000011);
-	end
+      assert(l == 64'b0000000000000000000000000000000100000000000000000000000010101010);
+      assert(m == 192'h000900120021);
+
+      assert(n == 3'b110);
+   end
 endmodule
