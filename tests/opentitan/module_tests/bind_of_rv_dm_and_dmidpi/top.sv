233e39ca9e1e058b1d67c395a76d8f1f3d0ea45e
Ryszard Różak <rrozak@antmicro.com>
2021-09-20 13:54:49 +0200

    Add test of bind of rv_dm and dmidpi
    Signed-off-by: Ryszard Różak <rrozak@antmicro.com>

---
+++ tests/opentitan/module_tests/bind_of_rv_dm_and_dmidpi/top.sv
@@ -0,0 +1,16 @@
+module top;
+     bind rv_dm dmidpi u_dmidpi (
+    .clk_i,
+    .rst_ni,
+    .dmi_req_valid,
+    .dmi_req_ready,
+    .dmi_req_addr   (dmi_req.addr),
+    .dmi_req_op     (dmi_req.op),
+    .dmi_req_data   (dmi_req.data),
+    .dmi_rsp_valid,
+    .dmi_rsp_ready,
+    .dmi_rsp_data   (dmi_rsp.data),
+    .dmi_rsp_resp   (dmi_rsp.resp),
+    .dmi_rst_n      (dmi_rst_n)
+  );
+endmodule // top
