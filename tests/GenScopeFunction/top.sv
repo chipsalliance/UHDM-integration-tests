236a8b6ba36fd19e0ef08fffc122c98871b4036f
Ryszard Różak <rrozak@antmicro.com>
2021-09-29 15:09:42 +0200

    Add label
    Signed-off-by: Ryszard Różak <rrozak@antmicro.com>

--- tests/GenScopeFunction/top.sv
+++ tests/GenScopeFunction/top.sv
@@ -1,13 +1,13 @@
 module top(output int o);
    parameter int unsigned Depth = 4;
 
-   if (Depth > 2) begin
+   if (Depth > 2) begin : gen_block
       function automatic int get1();
          return 1;
       endfunction // get1
 
       assign o = get1();
-   end
+   end : gen_block
    else
      assign o = 0;
 endmodule
