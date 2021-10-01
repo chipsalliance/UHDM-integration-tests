ca1fb8ab378b2ae25697ed44aad92ead3ec718e8
Ryszard Różak <rrozak@antmicro.com>
2021-10-01 11:30:11 +0200

    Change param name to CamelCase
    Signed-off-by: Ryszard Różak <rrozak@antmicro.com>

--- tests/CastInFunctionInGenBlock/top.sv
+++ tests/CastInFunctionInGenBlock/top.sv
@@ -1,10 +1,10 @@
 module top(output int o);
    parameter  int unsigned Depth  = 500;
-   localparam int unsigned PTR_WIDTH = 3;
+   localparam int unsigned PtrWidth = 3;
 
    if (Depth > 2) begin : gen_block
-      function automatic [PTR_WIDTH-1:0] get_casted_param();
-         logic [PTR_WIDTH-1:0] dec_tmp_sub = (PTR_WIDTH)'(Depth);
+      function automatic [PtrWidth-1:0] get_casted_param();
+         logic [PtrWidth-1:0] dec_tmp_sub = (PtrWidth)'(Depth);
          return dec_tmp_sub;
       endfunction
 
