3612af41eee1d974a70124ac4733c5d7786cf240
Ryszard Różak <rrozak@antmicro.com>
2021-10-13 16:37:13 +0200

    Use named ports
    Signed-off-by: Ryszard Różak <rrozak@antmicro.com>

--- tests/opentitan/module_tests/sw_test_status_if/top.sv
+++ tests/opentitan/module_tests/sw_test_status_if/top.sv
@@ -6,5 +6,8 @@ module top #(
   input logic [AddrWidth-1:0] addr, // Incoming addr.
   input logic [15:0] data           // Incoming data.
 );
-   sw_test_status_if #(AddrWidth) u_sw(clk_i, wr_valid, addr, data);
+   sw_test_status_if #(AddrWidth) u_sw(.clk_i(clk_i),
+                                       .wr_valid(wr_valid),
+                                       .addr(addr),
+                                       .data(data));
 endmodule // top
