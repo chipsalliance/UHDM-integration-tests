85bdc283df493065a201db15ab3fbdc668199a75
Ryszard Różak <rrozak@antmicro.com>
2021-12-01 16:25:05 +0100

    Add test of lc_ctrl module
    Signed-off-by: Ryszard Różak <rrozak@antmicro.com>

---
+++ tests/opentitan/opentitan-210214/module_tests/lc_ctrl/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <verilated_vcd_c.h>
+
+#define VL_DEBUG
+#include "Vlc_ctrl.h"
+#include "verilated.h"
+
+static vluint64_t main_time = 0;
+
+double
+sc_time_stamp()
+{
+  return main_time;
+}
+
+int main (int argc, char **argv) {
+  Verilated::commandArgs(argc, argv);
+  Vlc_ctrl *top = new Vlc_ctrl();
+
+  Verilated::traceEverOn(true);
+  VerilatedVcdC* tfp = new VerilatedVcdC;
+  top->trace(tfp, 99);
+  tfp->open("dump.vcd");
+
+  top->clk_i = 0;
+  top->rst_ni = 0;
+
+  while (!Verilated::gotFinish() && (main_time < 100)) {
+    top->eval();
+    tfp->dump(main_time);
+
+    main_time += 1;
+    top->clk_i ^= 1;
+    if (main_time > 10)
+      top->rst_ni = 1;
+
+    std::cout << "time: " << main_time
+      << " clk_i: " << (top->clk_i ? 1 : 0)
+      << " rst_ni: " << (top->rst_ni ? 1 : 0)
+      << std::endl;
+
+  }
+  top->final();
+  tfp->close();
+  delete top;
+
+  return 0;
+}
