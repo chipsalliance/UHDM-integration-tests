23d8f23d8305734e4849c1c2a47ac6bb02f34714
Rafal Kapuscik <rkapuscik@antmicro.com>
2022-06-10 11:15:30 +0200

    Update custom names in tests
    Signed-off-by: Rafal Kapuscik <rkapuscik@antmicro.com>

--- tests/fsm_single_always/yosys_script.tcl
+++ tests/fsm_single_always/yosys_script.tcl
@@ -1,6 +1,21 @@
-plugin -i uhdm
-read_uhdm -debug fsm_using_single_always.uhdm
-prep -top \fsm_using_single_always
+yosys -import
+plugin -i systemverilog
+yosys -import
+
+if {$::env(PARSER) == "surelog" } {
+	puts "Using Yosys read_uhdm command"
+	read_uhdm -debug fsm_using_single_always.uhdm
+} elseif {$::env(PARSER) == "yosys-plugin" } {
+	puts "Using Yosys read_systemverilog command"
+	read_systemverilog -debug $::env(TOP_FILE)
+} elseif {$::env(PARSER) == "yosys" } {
+	puts "Using Yosys read_verilog command"
+	read_verilog -debug $::env(TOP_FILE)
+} else {
+	error "Invalid PARSER"
+}
+
+prep -top \\fsm_using_single_always
 write_verilog
 write_verilog yosys.sv
 sim -clock clock -rstlen 10 -vcd dump.vcd
--- tests/genscope_cells/yosys_script.tcl
+++ tests/genscope_cells/yosys_script.tcl
@@ -1,7 +1,22 @@
-plugin -i uhdm
-read_uhdm -debug dut.uhdm
+yosys -import
+plugin -i systemverilog
+yosys -import
+
+if {$::env(PARSER) == "surelog" } {
+	 puts "Using Yosys read_uhdm command"
+	 read_uhdm -debug dut.uhdm
+} elseif {$::env(PARSER) == "yosys-plugin" } {
+	 puts "Using Yosys read_systemverilog command"
+	 read_systemverilog -debug $::env(TOP_FILE)
+} elseif {$::env(PARSER) == "yosys" } {
+	 puts "Using Yosys read_verilog command"
+	 read_verilog -debug $::env(TOP_FILE)
+} else {
+	 error "Invalid PARSER"
+}
+
 procs
 write_verilog
 write_verilog yosys.sv
-select -assert-count 1 \dut/gen_modules[1].module_in_genscope
-select -assert-count 1 \dut/gen_modules[0].module_in_genscope
+select -assert-count 1 \\dut/gen_modules\[1\].module_in_genscope
+select -assert-count 1 \\dut/gen_modules\[0\].module_in_genscope
