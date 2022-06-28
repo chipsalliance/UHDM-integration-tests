d0c1cd74f897016f0b1ce4e2aeb0e217aa9b360e
Krzysztof Bieganski <kbieganski@antmicro.com>
2022-06-28 10:27:40 +0200

    Add tests for `break` and `continue`
    Signed-off-by: Krzysztof Bieganski <kbieganski@antmicro.com>

--- tests/Break/top.sv
+++ tests/Break/top.sv
@@ -4,7 +4,7 @@ module top(output int a, output int b);
       b = 0;
       repeat(15) begin
          a = a + 10;
-         if(a > 100) continue;
+         if(a > 100) break;
          b = b + 5;
       end
    end
