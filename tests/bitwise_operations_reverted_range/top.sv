92d2e6894f6ba416988a2526ef584fadcf984903
Magdalena Andrys <mandrys@antmicro.com>
2022-11-07 15:58:10 +0100

    bitwise_operations_reverted_range: Add a rule for verible to ignore range order
    Signed-off-by: Magdalena Andrys <mandrys@antmicro.com>

--- tests/bitwise_operations_reverted_range/top.sv
+++ tests/bitwise_operations_reverted_range/top.sv
@@ -1,6 +1,6 @@
 module top
 (
-    input [0:3] in,
+    input [0:3] in, // verilog_lint: waive packed-dimensions-range-ordering
     output out_1, out_2, out_3, out_4, out_5, out_6, out_7, out_8, out_9, out_10,
            out_11, out_12, out_13, out_14, out_15, out_16, out_17, out_18
 );
