dfec59a03e1944970acaf8be22fd0d61dfe25acb
Kamil Rakoczy <krakoczy@antmicro.com>
2023-02-08 12:11:01 +0100

    Replace Casez and Casex with Casezx
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- tests/Casex/top.sv
+++
@@ -1,9 +0,0 @@
-module top(input logic [2:0] s, output logic [1:0] y);
-always_comb
- casex (s)
-  3'bxx1  : y  = 2'b01;
-  3'b01x  : y  = 2'b10;
-  3'b001  : y  = 2'b11;
-  default : y  = 2'b00;
-endcase
-endmodule : top
