477116ebe6539b07433cc2e16a4481997914e5fa
Kamil Rakoczy <krakoczy@antmicro.com>
2023-05-10 14:18:26 +0200

    Add AnonymousEnum test
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

---
+++ tests/AnonymousEnum/top.sv
@@ -0,0 +1,4 @@
+module top();
+enum logic [2:0] {e_idle, e_send_load} state_n, state_r; // verilog_lint: waive typedef-enums
+enum logic [3:0] {e_idle2, e_send_load2} state_n2, state_r2; // verilog_lint: waive typedef-enums
+endmodule
