908d21111fc8b6e060d0af0c7f7de077a0447bd2
Mariusz Glebocki <mglebocki@antmicro.com>
2023-08-29 16:21:21 +0200

    Fix lint issues.

--- tests/MultirangesStartNotFromZero/top.sv
+++ tests/MultirangesStartNotFromZero/top.sv
@@ -1,32 +1,32 @@
-module dut();
+module top();
    typedef struct packed {
       logic       v;
       logic       ready_and_rev;
       logic [66-1:0] data;
-  } bp_mem_ready_and_link_s;
+  } bp_mem_ready_and_link_t;
   logic [1-1:0][68-1:0]  mem_fwd_link_i;
   logic [1-1:0][68-1:0]  mem_rev_link_o;
   logic [1-1:0][68-1:0]  mem_dma_link_o;
-  typedef enum logic[2:0] {P=3'd0, W, E, N, S} Dirs;
-  bp_mem_ready_and_link_s [S:N][1-1:0] mem_ver_link_lo;
+  typedef enum logic[2:0] {p_e=3'd0, w_e, e_e, n_e, s_e} Dirs;
+  bp_mem_ready_and_link_t [s_e:n_e][1-1:0] mem_ver_link_lo;
 
-  assign mem_rev_link_o = mem_ver_link_lo[N];
-  assign mem_ver_link_lo[S] = mem_fwd_link_i;
-  assign mem_dma_link_o = mem_ver_link_lo[S];
+  assign mem_rev_link_o = mem_ver_link_lo[n_e];
+  assign mem_ver_link_lo[s_e] = mem_fwd_link_i;
+  assign mem_dma_link_o = mem_ver_link_lo[s_e];
    typedef struct packed {
       logic       v;
       logic       ready_and_rev;
       logic [66-1:0] data;
-  } bp_io_ready_and_link_s;
-  bp_io_ready_and_link_s [1-1:0][E:W] io_fwd_link_li;
-  bp_io_ready_and_link_s [1-1:0][E:W] io_fwd_link_lo;
-  bp_io_ready_and_link_s [1-1:0][E:W] io_rev_link_li;
-  bp_io_ready_and_link_s [1-1:0][E:W] io_rev_link_lo;
-  bp_io_ready_and_link_s [1-1:0][S:W] io_fwd_mesh_lo;
-  bp_io_ready_and_link_s [1-1:0][S:W] io_fwd_mesh_li;
-assign io_fwd_mesh_lo[0][E:W] = io_fwd_link_lo[0][E:W];
+  } bp_io_ready_and_link_t;
+  bp_io_ready_and_link_t [1-1:0][e_e:w_e] io_fwd_link_li;
+  bp_io_ready_and_link_t [1-1:0][e_e:w_e] io_fwd_link_lo;
+  bp_io_ready_and_link_t [1-1:0][e_e:w_e] io_rev_link_li;
+  bp_io_ready_and_link_t [1-1:0][e_e:w_e] io_rev_link_lo;
+  bp_io_ready_and_link_t [1-1:0][s_e:w_e] io_fwd_mesh_lo;
+  bp_io_ready_and_link_t [1-1:0][s_e:w_e] io_fwd_mesh_li;
+assign io_fwd_mesh_lo[0][e_e:w_e] = io_fwd_link_lo[0][e_e:w_e];
   for (genvar i = 0; i < 1; i++)
-    begin : cmd_link
-      assign io_fwd_link_li[i][E:W] = io_fwd_mesh_li[i][E:W];
+    begin : gen_cmd_link
+      assign io_fwd_link_li[i][e_e:w_e] = io_fwd_mesh_li[i][e_e:w_e];
     end
 endmodule
