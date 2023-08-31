ebbee9e87283a60548e25b5989e0b68fc605ad06
Ethan Mahintorabi <ethanmoon@google.com>
2023-08-31 23:24:48 +0000

    lint fixes
    Signed-off-by: Ethan Mahintorabi <ethanmoon@google.com>

--- tests/UnionParameter/top.sv
+++ tests/UnionParameter/top.sv
@@ -1,7 +1,7 @@
 package some_package; // verilog_lint: waive package-filename
 typedef union packed {
-  logic [3:0] bunn1;
-  logic [3:0] bunn2;
+  logic [3:0] bunn1_t;
+  logic [3:0] bunn2_t;
 } flimish_giant;
 
 parameter flimish_giant LovingHome = '{default: 1};
